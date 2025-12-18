5692786dbba6f1695295ef033ba5f1d57659d8cb
FakhriadiRasyaad <161245848+FakhriadiRasyaad@users.noreply.github.com>
2025-12-18 11:54:12 +0700

    Changed the main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from GUI.GUI import GUI
+from GUI.GUI import SportsDetectionApp
+import tkinter as tk
+
 if __name__ == '__main__':
-    splash = GUI()
-    splash.run()
+    root = tk.Tk()
+    app = SportsDetectionApp(root)
+    root.protocol("WM_DELETE_WINDOW", app.on_closing)
+    root.mainloop()
